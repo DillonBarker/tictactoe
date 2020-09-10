14c41df884f0425006dcc4ae6b32d190e7de7479
DillonBarker <barker.dill98@gmail.com>
2020-09-10 11:53:31 +0100

    writing plan, outlining what I need to do;

--- test_sample.py
+++
@@ -1,7 +0,0 @@
-from sample import func
-
-def test_answer():
-    assert func(3) == 5
-
-def test_func():
-    assert func(3) == 4
---
+++ test_tictactoe.py
@@ -0,0 +1,5 @@
+import pytest
+from tictactoe import *
+
+def test_func():
+    assert func(3) == 4
