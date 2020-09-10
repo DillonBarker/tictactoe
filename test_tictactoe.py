e9fa77c71ba7ff0d05b7923d340f61a92e6d6cf9
DillonBarker <barker.dill98@gmail.com>
2020-09-10 14:20:24 +0100

    adding a test and passing for game to be instantiated with two instatiated players X and O

--- test_tictactoe.py
+++ test_tictactoe.py
@@ -2,27 +2,40 @@ import pytest
 from tictactoe import *
 
 
-def test_board_class():
-    board = Board()
-    assert board.the_board == ["-","-","-",
-                               "-","-","-",
-                               "-","-","-"]
-    assert board.the_board != ["dog","-","-",
-                                 "-","-","-",
-                                 "-","-","-"]
-
-def test_game_class():
-    board = Board()
-    game = Game(board)
-    assert game.board.the_board == ["-","-","-",
+class TestBoard:
+    def test_board_class(self):
+        board = Board()
+        assert board.the_board == ["-","-","-",
+                                "-","-","-",
+                                "-","-","-"]
+        assert board.the_board != ["dog","-","-",
                                     "-","-","-",
                                     "-","-","-"]
-    assert isinstance(game.board, Board) 
 
-def test_player_class():
-    player_1 = Player("X")
-    assert player_1.name == "X"
-    assert player_1.score == 0
-    player_2 = Player("O")
-    assert player_2.name == "O"
-    assert player_2.score == 0
+class TestGame:
+    def test_game_class(self):
+        board = Board()
+        player_1 = Player("X")
+        player_2 = Player("O")
+        game = Game(board, player_1, player_2)
+        assert game.board.the_board == ["-","-","-",
+                                        "-","-","-",
+                                        "-","-","-"]
+        assert isinstance(game.board, Board)
+
+    def test_game_class_has_player(self):
+        player_1 = Player("X")
+        player_2 = Player("O")
+        board = Board()
+        game = Game(board, player_1, player_2)
+        assert isinstance(game.player_1, Player)
+
+class TestPlayer:
+    def test_player_class(self):
+        player_1 = Player("X")
+        assert player_1.name == "X"
+        assert player_1.score == 0
+        player_2 = Player("O")
+        assert player_2.name == "O"
+        assert player_2.score == 0
+
--- tictactoe.py
+++ tictactoe.py
@@ -10,6 +10,8 @@ class Player:
         self.score = 0
 
 class Game():
-    def __init__(self, board):
+    def __init__(self, board, player_1, player_2):
         self.board = Board()
+        self.player_1 = Player("X")
+        self.player_2 = Player("O")
 
