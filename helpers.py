992aa29acddb6ff0059af653e368e5bcef65005a
DillonBarker <barker.dill98@gmail.com>
2020-09-11 15:29:50 +0100

    happy with the logic

--- helpers.py
+++
@@ -1,9 +0,0 @@
-import pytest
-from tictactoe import *
-
-class Helpers:
-    def add_players_and_start_game(self):
-        board = Board()
-        player_1 = Player("X")
-        player_2 = Player("O")
-        self.game = Game(board, player_1, player_2)
