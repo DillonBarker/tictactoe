44b4520a223be0a2f1896d4e85ea552d08deb3b7
DillonBarker <barker.dill98@gmail.com>
2020-09-11 16:08:23 +0100

    added interface and added separation for the array of arrays so its easier to see!

--- interface.py
+++ interface.py
@@ -7,13 +7,13 @@ class PlayGame:
     def start_game(self):
         while self.game.game_over == False:
             print("Welcome to TicTacToe by Dillon Barker")
-            print(self.game.board.the_board)
+            print(*self.game.board.the_board, sep="\n")
             print("Make a move " + str(self.game.turncontrol.turn.name))
             for x in [1,2,3,4,5,6,7,8,9]:
                 x = input("Enter an x coord: ")
                 y = input("Enter an y coord: ")
                 self.game.player_move(int(x), int(y))
-                print(self.game.board.the_board)
+                print(*self.game.board.the_board, sep="\n")
                 print("Make a move " + str(self.game.turncontrol.turn.name))
                 if self.game.game_over == True:
                     break
